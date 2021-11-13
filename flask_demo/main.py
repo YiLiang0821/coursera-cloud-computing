1d178b50827696af9a79eb59479f08b3cce2d562
Yi Liang Chen <kaocyliang@gmail.com>
2021-11-13 18:55:32 +0800

    tt1

--- flask_demo/main.py
+++ flask_demo/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello CD with Cloud Build!!!! test3333'
+    return 'CLoud Build testing'
 
 @app.route('/echo/<name>')
 def echo(name):
